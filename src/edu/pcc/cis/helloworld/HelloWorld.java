8a79d308c7891210aa54852bbeae5dd1093bd22f
Elizabeth Herman <elizabeth.herman2@pcc.edu>
2022-11-07 19:38:45 -0800

    Added print date capabilities.

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,6 +1,8 @@
 package edu.pcc.cis.helloworld;
+import java.util.Date;
 /**
- * A class for greeting the world.
+ * A class for greeting the world,
+ * and printing the date.
  *
  * @author Elizabeth Herman
  * @version 2022.11.07
@@ -8,5 +10,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
