605e57909c37735730ac8bc101863d71aa9ee3ac
Elizabeth Herman <elizabeth.herman2@pcc.edu>
2022-11-07 19:32:24 -0800

    Made the main method say hello.

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,12 @@
 package edu.pcc.cis.helloworld;
-
+/**
+ * A class for greeting the world.
+ *
+ * @author Elizabeth Herman
+ * @version 2022.11.07
+ */
 public class HelloWorld {
-    public HelloWorld() {
-        sayHello();
-    }
-    public String sayHello() {
-        return "Hello world!";
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
