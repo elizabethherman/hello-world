269b4ec9194334639d36a8651b0cfe883919fae8
Elizabeth Herman <elizabeth.herman2@pcc.edu>
2022-11-07 18:39:29 -0800

    Added a method to say hello.

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
 public class HelloWorld {
+    public HelloWorld() {
+        sayHello();
+    }
+    public String sayHello() {
+        return "Hello world!";
+    }
 }
